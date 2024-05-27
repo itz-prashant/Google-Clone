8367cdfd33e45085604c655a2987123108422430
Prashant gupta <prashantguptaofficial007@gmail.com>
2024-05-27 17:27:01 +0530

    axios install

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Home from "./components/Home"
+import SearchResult from "./components/SearchResult"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+    <Routes>
+      <Route  path='/'  element={<Home />}/>
+      <Route  path='/'  element={<SearchResult />}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
